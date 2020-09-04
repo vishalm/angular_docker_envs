1e1a2334a51d11d9a4287a13fce56c54fffea632
asdf <asdf@asdf.asdf>
2020-09-04 23:17:31 +0200

    added stuff for docker envs - see readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  public dockerenv: string;
+
   title = 'angular-env-docker';
+
+  constructor() {
+    this.dockerenv = "ctor"
+  }
+
 }
